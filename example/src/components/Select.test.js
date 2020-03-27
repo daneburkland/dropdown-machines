618343ec623a87587f868edabf97cb902588510a
Dane Burkland <dane.burkland@centro-201716.centro.net>
2020-03-26 20:57:29 -0700

    initial model based React example tests

--- example/src/components/Select.test.js
+++ example/src/components/Select.test.js
@@ -1,130 +1,200 @@
+import { Machine } from "xstate";
 import React from "react";
-import { render, fireEvent, wait } from "@testing-library/react";
+import { render, fireEvent, wait, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { createModel } from "@xstate/test";
 import Select from "./Select";
 
 import items from "../items";
 
-const Selects = [
-  {
-    description: "Basic Select",
-    getComponent: props => <Select items={items} {...props} />
+const selectMachine = Machine({
+  id: "select",
+  initial: "closed",
+  states: {
+    open: {
+      meta: {
+        test: ({ getAllByTestId }: any) => {
+          const options = getAllByTestId("option");
+          expect(options).toHaveLength(11);
+        }
+      },
+      on: {
+        CLICK_TRIGGER: "closed",
+        KEY_DOWN_SELECT_ESC: "closed"
+      }
+    },
+    closed: {
+      meta: {
+        test: ({ queryByTestId }: any) => {
+          wait(() => {
+            expect(queryByTestId("listBox")).not.toBeVisible();
+          });
+        }
+      },
+      on: {
+        CLICK_TRIGGER: "open",
+        KEY_DOWN_SELECT_SPACE: "open"
+      }
+    }
   }
-];
+});
 
-Selects.forEach(({ description, getComponent }) => {
-  describe(description, () => {
-    test("opens and sets correct value with click", () => {
-      const { getByTestId, getAllByTestId, getByText, findByText } = render(
-        getComponent()
-      );
-
+describe("Select component", () => {
+  const testModel = createModel(selectMachine).withEvents({
+    KEY_DOWN_SELECT_SPACE: ({ getByTestId }) => {
       const selectElement = getByTestId("select");
-
-      fireEvent.click(selectElement);
-      const options = getAllByTestId("option");
-      expect(options).toHaveLength(items.length);
-
-      fireEvent.keyDown(selectElement, {
-        key: "ArrowDown",
-        keyCode: 40
-      });
-      fireEvent.keyDown(selectElement, {
-        key: "Enter",
-        keyCode: 13
-      });
-
-      expect(selectElement.textContent).toBe("first");
-    });
-
-    test("targets and selects correct value with keyboard(arrow keys)", () => {
-      const { getByTestId, getAllByTestId, getByText } = render(getComponent());
-
-      const selectElement = getByTestId("select");
-
       fireEvent.focus(selectElement);
       fireEvent.keyDown(selectElement, {
         key: "Space",
         keyCode: 32
       });
-      const options = getAllByTestId("option");
-      expect(options).toHaveLength(items.length);
-
-      fireEvent.keyDown(selectElement, {
-        key: "ArrowDown",
-        keyCode: 40
-      });
-      fireEvent.keyDown(selectElement, {
-        key: "Enter",
-        keyCode: 13
-      });
-
-      expect(selectElement.textContent).toBe("first");
-    });
-
-    test("targets and selects correct value with keyboard(fuzzy find navigation)", () => {
-      const { getByTestId, getAllByTestId, getByText } = render(getComponent());
-
+    },
+    KEY_DOWN_SELECT_ESC: ({ getByTestId }) => {
       const selectElement = getByTestId("select");
-
-      fireEvent.click(selectElement);
-
-      fireEvent.keyDown(selectElement, {
-        key: "t",
-        keyCode: 84
-      });
-      fireEvent.keyDown(selectElement, {
-        key: "h",
-        keyCode: 72
-      });
-      fireEvent.keyDown(selectElement, {
-        key: "Enter",
-        keyCode: 13
-      });
-
-      expect(selectElement.textContent).toBe("third");
-    });
-
-    test("closes on `esc`", () => {
-      const {
-        getByTestId,
-        getAllByTestId,
-        queryAllByTestId,
-        queryByTestId
-      } = render(getComponent());
-
-      const selectElement = getByTestId("select");
-
-      fireEvent.click(selectElement);
-
-      const options = getAllByTestId("option");
-      expect(options).toHaveLength(items.length);
-
+      fireEvent.focus(selectElement);
       fireEvent.keyDown(selectElement, {
         key: "Escape",
         keyCode: 27
       });
-
-      wait(() => {
-        expect(queryByTestId("listBox")).not.toBeVisible();
-      });
-    });
-
-    test("auto targets first option with autoTargetFirstItem", () => {
-      const { getByTestId, getAllByTestId, getByText } = render(
-        getComponent({ autoTargetFirstItem: true })
-      );
-
+    },
+    CLICK_TRIGGER: ({ getByTestId }) => {
       const selectElement = getByTestId("select");
-
       fireEvent.click(selectElement);
+    }
+  });
 
-      fireEvent.keyDown(selectElement, {
-        key: "Enter",
-        keyCode: 13
+  const testPlans = testModel.getSimplePathPlans();
+  console.log("testPlan", testPlans);
+  testPlans.forEach(plan => {
+    describe(plan.description, () => {
+      afterEach(cleanup);
+      plan.paths.forEach(path => {
+        it(path.description, () => {
+          const rendered = render(<Select items={items} />);
+          return path.test(rendered);
+        });
       });
-
-      expect(selectElement.textContent).toBe("first");
     });
   });
+
+  it("coverage", () => {
+    testModel.testCoverage();
+  });
 });
+
+// Selects.forEach(({ description, getComponent }) => {
+//   describe(description, () => {
+//     test("opens and sets correct value with click", () => {
+//       const { getByTestId, getAllByTestId, getByText, findByText } = render(
+//         getComponent()
+//       );
+
+//       const selectElement = getByTestId("select");
+
+//       fireEvent.click(selectElement);
+//       const options = getAllByTestId("option");
+//       expect(options).toHaveLength(items.length);
+
+//       fireEvent.keyDown(selectElement, {
+//         key: "ArrowDown",
+//         keyCode: 40
+//       });
+//       fireEvent.keyDown(selectElement, {
+//         key: "Enter",
+//         keyCode: 13
+//       });
+
+//       expect(selectElement.textContent).toBe("first");
+//     });
+
+//     test("targets and selects correct value with keyboard(arrow keys)", () => {
+//       const { getByTestId, getAllByTestId, getByText } = render(getComponent());
+
+//       const selectElement = getByTestId("select");
+
+//       fireEvent.focus(selectElement);
+//       fireEvent.keyDown(selectElement, {
+//         key: "Space",
+//         keyCode: 32
+//       });
+//       const options = getAllByTestId("option");
+//       expect(options).toHaveLength(items.length);
+
+//       fireEvent.keyDown(selectElement, {
+//         key: "ArrowDown",
+//         keyCode: 40
+//       });
+//       fireEvent.keyDown(selectElement, {
+//         key: "Enter",
+//         keyCode: 13
+//       });
+
+//       expect(selectElement.textContent).toBe("first");
+//     });
+
+//     test("targets and selects correct value with keyboard(fuzzy find navigation)", () => {
+//       const { getByTestId, getAllByTestId, getByText } = render(getComponent());
+
+//       const selectElement = getByTestId("select");
+
+//       fireEvent.click(selectElement);
+
+//       fireEvent.keyDown(selectElement, {
+//         key: "t",
+//         keyCode: 84
+//       });
+//       fireEvent.keyDown(selectElement, {
+//         key: "h",
+//         keyCode: 72
+//       });
+//       fireEvent.keyDown(selectElement, {
+//         key: "Enter",
+//         keyCode: 13
+//       });
+
+//       expect(selectElement.textContent).toBe("third");
+//     });
+
+//     test("closes on `esc`", () => {
+//       const {
+//         getByTestId,
+//         getAllByTestId,
+//         queryAllByTestId,
+//         queryByTestId
+//       } = render(getComponent());
+
+//       const selectElement = getByTestId("select");
+
+//       fireEvent.click(selectElement);
+
+//       const options = getAllByTestId("option");
+//       expect(options).toHaveLength(items.length);
+
+//       fireEvent.keyDown(selectElement, {
+//         key: "Escape",
+//         keyCode: 27
+//       });
+
+//       wait(() => {
+//         expect(queryByTestId("listBox")).not.toBeVisible();
+//       });
+//     });
+
+//     test("auto targets first option with autoTargetFirstItem", () => {
+//       const { getByTestId, getAllByTestId, getByText } = render(
+//         getComponent({ autoTargetFirstItem: true })
+//       );
+
+//       const selectElement = getByTestId("select");
+
+//       fireEvent.click(selectElement);
+
+//       fireEvent.keyDown(selectElement, {
+//         key: "Enter",
+//         keyCode: 13
+//       });
+
+//       expect(selectElement.textContent).toBe("first");
+//     });
+//   });
+// });
